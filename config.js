106584fbc3b458012f27c4d1b5f3a23851a13bd3
nhaideptrai <96351989+nhaideptrai@users.noreply.github.com>
2021-12-19 02:03:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Nguyễn Nhật Hằng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng Hằng đã yêu Nhái rồi đúng không 🥰',
+    desc: 'Phải chăng em đã thương anh rồi đúng không ',
+    btnYes: 'Yêu Nhái nhiều lắm <33',
+    btnNo: 'đéo yêu m ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Hằng lại yêu Nhái <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì em yêu nhái nhiều lắm 🥺 <33333333',
+    mess: 'Nhái biết mà 🥰. Yêu bạn nhiều lắm 😘😘',
+    messDesc: 'Tối nay 7h, Nhái qua đón Hằng đi ăn thịt khủng long nho :3.',
+    btnAccept: 'Dạ babi iu <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
